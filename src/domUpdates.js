4545f100fe688c78aa2bb2d98964bfa9b3d3e7eb
Taranveer Singh <taranveersingh93@gmail.com>
2023-06-07 20:46:15 -0400

    Make login box dynamic

---
+++ src/domUpdates.js
@@ -0,0 +1,23 @@
+import {
+  loginBtn,
+  customerLoginRadio,
+  managerLoginRadio,
+  usernameInput,
+  passwordInput
+} from './scripts'
+
+const activateLoginBtn = () => {
+  const userTypeSelected = customerLoginRadio.checked || managerLoginRadio.checked;
+  const userInputEntered = usernameInput.value && passwordInput.value;
+  if (userTypeSelected && userInputEntered) {
+    loginBtn.disabled = false;
+    loginBtn.classList.add('active-login');
+  } else {
+    loginBtn.disabled = true;
+    loginBtn.classList.remove('active-login');
+  } 
+}
+
+export {
+  activateLoginBtn
+}
--- src/scripts.js
+++ src/scripts.js
@@ -2,4 +2,20 @@
 import './css/styles.css';
 import './images/turing-logo.png'
 
+// DOM elements
+const loginBtn = document.querySelector('.login-button');
+const customerLoginRadio = document.querySelector('#customerLogin');
+const managerLoginRadio = document.querySelector('#managerLogin');
+const usernameInput = document.querySelector('.username-input');
+const passwordInput = document.querySelector('.password-input');
+
 // Event listeners
+
+// Exports
+export {
+  loginBtn,
+  customerLoginRadio,
+  managerLoginRadio,
+  usernameInput,
+  passwordInput
+}
