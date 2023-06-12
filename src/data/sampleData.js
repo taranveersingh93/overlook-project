542f7d7cd2371e450072ff3b93b5bee95298e5d9
Taranveer Singh <taranveersingh93@gmail.com>
2023-06-12 15:21:36 -0400

    Add error message for no rooms found

---
+++ src/data/sampleData.js
@@ -0,0 +1,16 @@
+const sampleBookings = [
+    {"id":"5fwrgu4i7k55hl6t5","userID":13,"date":"2022/01/24","roomNumber":24},
+    {"id":"5fwrgu4i7k55hl6t6","userID":13,"date":"2022/01/10","roomNumber":12},
+    {"id":"5fwrgu4i7k55hl6t7","userID":20,"date":"2022/02/16","roomNumber":7},
+    {"id":"5fwrgu4i7k55hl6t8","userID":13,"date":"2022/02/05","roomNumber":12},
+    {"id":"5fwrgu4i7k55hl6t9","userID":38,"date":"2023/12/14","roomNumber":14},
+    {"id":"5fwrgu4i7k55hl6ta","userID":25,"date":"2022/01/11","roomNumber":9},
+    {"id":"5fwrgu4i7k55hl6tb","userID":49,"date":"2022/02/06","roomNumber":5}
+  ];
+
+const sampleUser = {"id":13,"name":"Christina Kulas"};
+
+export {
+  sampleBookings,
+  sampleUser
+}
